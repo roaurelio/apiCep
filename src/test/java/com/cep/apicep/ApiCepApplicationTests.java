ecfdeb89d8a28d3aac5b294fbd1f06a3b7ff588f
Rosana <ro.aurelio16@gmail.com>
2020-03-29 22:13:48 -0300

    Testes

--- src/test/java/com/cep/apicep/ApiCepApplicationTests.java
+++ src/test/java/com/cep/apicep/ApiCepApplicationTests.java
@@ -1,13 +1,64 @@
 package com.cep.apicep;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URISyntaxException;
+
+import javax.validation.constraints.AssertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.cep.apicep.model.Cep;
+import com.cep.apicep.service.CepService;
+import com.cep.apicep.service.restTemplate.RestTemplateCep;
 
 @SpringBootTest
 class ApiCepApplicationTests {
 
+	
+	@Mock
+	private RestTemplateCep restTemplateCep;
+	
+	@Autowired
+	private CepService cepService;
+	
 	@Test
-	void contextLoads() {
+	void buscarCepSucesso() {
+		
+		try {
+			Cep cep = cepService.buscarCriarCep("29102380");
+			assertThat(cep).isNotNull();
+			assertThat(cep.getCep()).isNotNull();
+			
+			System.out.println(cep.getBairro());
+			System.out.println(cep.getLogradouro());
+			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	void buscarCepErroEntrada() {
+		Boolean thrown = false;
+		try {
+			cepService.buscarCriarCep("2910238");
+			
+		} catch (HttpClientErrorException | URISyntaxException e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+		
+	}
+
+	private void assertTrue(Boolean thrown) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
